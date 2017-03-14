b311b21438e0300f5de677724fa8e82ac691c2ba
Josip Anic <janic@pinkstongroup.com>
2017-03-14 11:36:55 +0100

    Set navigation width

--- app/components/Navigation/Ul.js
+++ app/components/Navigation/Ul.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Ul = styled.ul`
   list-style: none;
+  max-width: 300px;
 `;
 
 export default Ul;
--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -5,17 +5,23 @@
 */
 
 import React, { PropTypes } from 'react';
+import Nav from './Nav';
 import NavList from './NavList';
 
-const Navigation = ({ navItems, handleRoute }) => (
-  <nav>
+const Navigation = ({ navItems, handleRoute, isNavActive }) => (
+  <Nav active={isNavActive}>
     <NavList navItems={navItems} handleRoute={handleRoute} />
-  </nav>
+  </Nav>
 );
 
 Navigation.propTypes = {
   navItems: PropTypes.array.isRequired,
   handleRoute: PropTypes.func.isRequired,
+  isNavActive: PropTypes.bool.isRequired,
+};
+
+Navigation.defaultProps = {
+  isNavActive: true,
 };
 
 export default Navigation;
