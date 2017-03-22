27183057ebbf1c447a74bd35d0491704f790a9f4
Nenad Brcic <neno@neno.ch>
2017-03-22 10:46:38 +0100

    fix(Header): fix bg color

--- app/components/Gallery/Wrapper.js
+++ app/components/Gallery/Wrapper.js
@@ -6,7 +6,6 @@ const Wrapper = styled.div`
   flex-flow: row wrap;
   justify-content: flex-start;
   align-content: flex-start;
-  padding: 1rem 0;
   background: white;
   margin-bottom: 80px;
 `;
